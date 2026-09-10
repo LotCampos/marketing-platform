bd5401066cdcdb067d692c2dd257a300d4cf8a77
Lot Campos <marketing@uvcado.com.mx>
2026-09-10 14:45:45 -0600

    commercial: expose agreement service

--- backend/commercial/services/__init__.py
+++ backend/commercial/services/__init__.py
@@ -1,29 +1,14 @@
-from .opportunity_service import (
-    OpportunityCreateData,
-    OpportunityService,
-)
-from .prospect_conversion_service import (
-    ProspectConversionResult,
-    ProspectConversionService,
-)
-from .prospect_service import (
-    OptimisticLockError,
-    ProspectCreateData,
-    ProspectService,
-)
-from .quotation_service import (
-    QuotationCreateData,
-    QuotationItemCreateData,
-    QuotationService,
-)
-from .quotation_pdf_service import (
-    QuotationPDFService,
-)
-from .service_request_service import (
-    ServiceRequestService,
-)
+from .agreement_service import AgreementCreateData, AgreementService
+from .opportunity_service import OpportunityCreateData, OpportunityService
+from .prospect_conversion_service import ProspectConversionResult, ProspectConversionService
+from .prospect_service import OptimisticLockError, ProspectCreateData, ProspectService
+from .quotation_service import QuotationCreateData, QuotationItemCreateData, QuotationService
+from .quotation_pdf_service import QuotationPDFService
+from .service_request_service import ServiceRequestService
 
 __all__ = [
+    "AgreementCreateData",
+    "AgreementService",
     "OptimisticLockError",
     "OpportunityCreateData",
     "OpportunityService",
